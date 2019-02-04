c9be5f9b59480c1348a94b2ccc72e8b006e500ff
Jairo <jairobernaldev@gmail.com>
2019-02-04 09:45:48 +0100

    Consulta regalos reyes.

--- phpIncludes/regalosreyes.php
+++ phpIncludes/regalosreyes.php
@@ -1,15 +1,22 @@
 <?php
-    $queryRegalos = mysqli_query($conexion, "SELECT count(*) AS co FROM regalos re WHERE re.idRegalos IN (SELECT idRegalos FROM pedidos)");
-    echo "<tr>";
-    echo "<td> ffff</td>";
+    for($i = 1; $i<4; $i++){
+        $queryRegalos = mysqli_query($conexion, "SELECT r.Nombre, count(e.idRey) AS co FROM rey r, regalos re, entregas e WHERE r.idRey = e.idRey AND re.idRegalos = e.idRegalos AND r.idRey = $i");
+        $dato = mysqli_fetch_assoc($queryRegalos);
+        echo "<tr>";
+        echo "<td>".$dato["Nombre"]."</td>";
+        echo "<td>".$dato["co"]."</td>";
+        echo "</tr>";
+    }
+    /*echo "<tr>";
+    echo "<td>".$dato["Nombre"]."</td>";
     echo "<td>".$dato["co"]."</td>";
     echo "</tr>";
     echo "<tr>";
-    echo "<td> ffff</td>";
+    echo "<td>".$dato["Nombre"]."</td>";
     echo "<td>".$dato["co"]."</td>";
     echo "</tr>";
     echo "<tr>";
-    echo "<td> ffff</td>";
+    echo "<td>".$dato["Nombre"]."</td>";
     echo "<td>".$dato["co"]."</td>";
-    echo "</tr>";
+    echo "</tr>";*/
 ?>
