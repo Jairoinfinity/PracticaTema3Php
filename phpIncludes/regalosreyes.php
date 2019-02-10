6e4c683b1cba2493bda617842a04dbaa362b97fc
Jairo <jairobernaldev@gmail.com>
2019-02-10 17:20:13 +0100

    Retoques de ultima hora.

--- phpIncludes/regalosreyes.php
+++ phpIncludes/regalosreyes.php
@@ -1,10 +1,10 @@
 <?php
-    for($i = 1; $i<4; $i++){
-        $queryRegalos = mysqli_query($conexion, "SELECT r.Nombre ,count(re.idRegalo) AS co FROM regalo re, rey r WHERE re.idRey = $i AND r.idRey = $i");
-        $dato = mysqli_fetch_assoc($queryRegalos);
-        echo "<tr>";
-        echo "<td>".$dato["Nombre"]."</td>";
-        echo "<td>".$dato["co"]."</td>";
-        echo "</tr>";
-    }
+    $queryRegalos = mysqli_query($conexion, "SELECT r.Nombre ,count(re.idRegalo) AS co FROM regalo re, rey r WHERE re.idRey = r.idRey GROUP BY re.idRey");
+        while($dato = mysqli_fetch_assoc($queryRegalos)){
+            echo "<tr>";
+            echo "<td>".$dato["Nombre"]."</td>";
+            echo "<td>".$dato["co"]."</td>";
+            echo "</tr>";
+        }
+        
 ?>
